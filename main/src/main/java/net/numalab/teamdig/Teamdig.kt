811705fa5fa83b7b51a3448348ffc1b5a67f0a2a
bun133 <bun133.work@gmail.com>
2022-02-05 20:42:04 +0900

    チケット1:環境準備 完了

---
+++ main/src/main/java/net/numalab/teamdig/Teamdig.kt
@@ -0,0 +1,14 @@
+package net.numalab.teamdig
+
+import dev.kotx.flylib.flyLib
+import org.bukkit.plugin.java.JavaPlugin
+
+class Teamdig : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
